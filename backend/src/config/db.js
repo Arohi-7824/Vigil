4fdfa536ff322f6ef0f66f0d1d9dbd3aa93cbeb5
Arohi singh <arohisingh@Arohis-MacBook-Air.local>
2026-07-06 23:01:55 +0530

    upadted db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,14 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()
-  .then(() => console.log("✅ DB connected"))
+  .then(() => console.log("✅ Connected to Neon PostgreSQL"))
   .catch(err => console.error("❌ DB connection error:", err.message));
 
 module.exports = pool;
